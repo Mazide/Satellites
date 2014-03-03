e004282d6be9f4f94aafb63aa7ec2405b10075b5
Nikita Demidov <ndemidov@ovpnbr11.horis.ru>
2014-03-03 13:05:30 +0400

    Initial Commit

---
+++ Satellites/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Satellites
+//
+//  Created by Nikita Demidov on 03/03/14.
+//  Copyright (c) 2014 HORIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
